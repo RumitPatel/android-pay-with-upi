89b50022ecb3af9d7852eaf6590fd896283dca4f
RumitEmed <rumit.emed@gmail.com>
2023-03-21 20:02:43 +0530

    Code clean up.

--- app/src/main/java/com/rum/upipaymentapp/MainActivity.kt
+++ app/src/main/java/com/rum/upipaymentapp/MainActivity.kt
@@ -18,7 +18,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Face
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
