808380be55b18d458404bd603e7ef7e5cf969d3b
RumitEmed <rumit.emed@gmail.com>
2023-03-21 19:39:29 +0530

    Code clean up.

--- app/src/main/java/com/rum/upipaymentapp/MainActivity.kt
+++ app/src/main/java/com/rum/upipaymentapp/MainActivity.kt
@@ -27,12 +27,10 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.rum.upipaymentapp.ui.theme.UPIPaymentAppTheme
@@ -197,20 +195,5 @@ class MainActivity : ComponentActivity() {
             }
             return false
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!", modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    UPIPaymentAppTheme {
-        Greeting("Android")
     }
 }
