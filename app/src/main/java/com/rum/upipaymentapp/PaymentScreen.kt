b9e64693213f2a38dff4011c336121a877937356
RumitPatel <rumit.patel5@gmail.com>
2023-03-22 12:18:38 +0530

    Formatted.

--- app/src/main/java/com/rum/upipaymentapp/PaymentScreen.kt
+++ app/src/main/java/com/rum/upipaymentapp/PaymentScreen.kt
@@ -1,6 +1,5 @@
 package com.rum.upipaymentapp
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
@@ -21,13 +20,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.rum.upipaymentapp.ui.custom_views.MyText
